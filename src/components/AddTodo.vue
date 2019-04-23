02f20864fe18681e90df8a1590cce2c716d5034b
HenriqueMitsuo <henrique.informatica123@gmail.com>
2019-04-23 15:40:00 -0300

    Initial Commit + Todo-App (Without HTTP request)

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input type="text" name="title" placeholder="Add todo..." v-model="title">
+            <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+import uuid from 'uuid';
+
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addTodo(e) {
+            e.preventDefault();
+            if(this.title !== ''){
+                const newTodo = {
+                    id: uuid.v4(),
+                    title: this.title,
+                    completed: false
+                }
+                this.$emit('add-todo', newTodo);
+                this.title = '';
+            }
+            else{
+                alert("Preencha o campo!!")
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form{
+        display: flex;
+    }
+    input[type="text"]{
+        flex: 10;
+        padding: 5px;
+    }
+    input[type="submit"]{
+        flex: 2;
+    }
+</style>
+
+
